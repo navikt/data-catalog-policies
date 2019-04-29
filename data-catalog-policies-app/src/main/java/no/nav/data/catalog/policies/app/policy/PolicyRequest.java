df8ac38d347cdb15c55dc3dcf9d8e7248328a6b0
Ketill Fenne <ketill.fenne@gmail.com>
2019-04-29 20:25:18 +0200

    Fjernet JsonCreator constructpr

--- data-catalog-policies-app/src/main/java/no/nav/data/catalog/policies/app/policy/PolicyRequest.java
+++ data-catalog-policies-app/src/main/java/no/nav/data/catalog/policies/app/policy/PolicyRequest.java
@@ -1,28 +1,17 @@
 package no.nav.data.catalog.policies.app.policy;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Builder;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class PolicyRequest {
     private Long legalBasisId;
     private String legalBasisDescription;
     private Long purposeId;
     private Long informationTypeId;
-
-    @JsonCreator
-    public PolicyRequest(
-            @JsonProperty(value = "legalBasisId", required = true) Long legalBasisId,
-            @JsonProperty(value = "legalBasisDescription", required = true) String legalBasisDescription,
-            @JsonProperty(value = "purposeId", required = true) Long purposeId,
-            @JsonProperty(value = "informationTypeId", required = true) Long informationTypeId) {
-        this.legalBasisId = legalBasisId;
-        this.purposeId = purposeId;
-        this.informationTypeId = informationTypeId;
-        this.legalBasisDescription = legalBasisDescription;
-    }
 }
