52830d141a415deaf642558a0301d7dd22150c1e
Ketill Fenne <ketill.fenne@gmail.com>
2019-04-30 12:24:36 +0200

    Bort med Policy service

--- data-catalog-policies-app/src/main/java/no/nav/data/catalog/policies/app/policy/rest/PolicyRestController.java
+++ data-catalog-policies-app/src/main/java/no/nav/data/catalog/policies/app/policy/rest/PolicyRestController.java
@@ -4,11 +4,15 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+import no.nav.data.catalog.policies.app.common.exceptions.DataCatalogPoliciesNotFoundException;
 import no.nav.data.catalog.policies.app.policy.PolicyRequest;
 import no.nav.data.catalog.policies.app.policy.entities.LegalBasis;
 import no.nav.data.catalog.policies.app.policy.entities.Policy;
 import no.nav.data.catalog.policies.app.policy.entities.Purpose;
-import no.nav.data.catalog.policies.app.policy.service.PolicyService;
+import no.nav.data.catalog.policies.app.policy.repository.LegalBasisRepository;
+import no.nav.data.catalog.policies.app.policy.repository.PolicyRepository;
+import no.nav.data.catalog.policies.app.policy.repository.PurposeRepository;
+import no.nav.data.catalog.policies.app.policy.mapper.PolicyMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -17,14 +21,23 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @CrossOrigin
 @Api(value = "Data Catalog Policies", description = "REST API for Policies", tags = { "Policies" })
 public class PolicyRestController {
+    @Autowired
+    private PolicyMapper mapper;
 
     @Autowired
-    private PolicyService service;
+    private PolicyRepository policyRepository;
+
+    @Autowired
+    private LegalBasisRepository legalBasisRepository;
+
+    @Autowired
+    private PurposeRepository purposeRepository;
 
     @ApiOperation(value = "Get all Policies", tags = { "Policies" })
     @ApiResponses(value = {
@@ -32,7 +45,7 @@ public class PolicyRestController {
                 @ApiResponse(code = 500, message = "Internal server error")})
     @GetMapping("/policy")
     public Page<Policy> getPolicies(Pageable pageable) {
-        return service.getPolicies(pageable);
+        return policyRepository.findAll(pageable);
     }
 
     @ApiOperation(value = "Create Policy", tags = { "Policies" })
@@ -43,9 +56,8 @@ public class PolicyRestController {
     @PostMapping("/policy")
     @ResponseStatus(HttpStatus.CREATED)
     public Policy createPolicy(@Valid @RequestBody PolicyRequest policyRequest) {
-        Policy policy = service.createPolicy(policyRequest);
-        return policy;
-    }
+        Policy policy = mapper.mapRequestToPolicy(policyRequest, null);
+        return policyRepository.save(policy);    }
 
     @ApiOperation(value = "Get Policy", tags = { "Policies" })
     @ApiResponses(value = {
@@ -54,7 +66,11 @@ public class PolicyRestController {
             @ApiResponse(code = 500, message = "Internal server error")})
     @GetMapping("/policy/{id}")
     public Policy getPolicy(@PathVariable Long id) {
-        return service.getPolicy(id);
+        Optional<Policy> optionalPolicy = policyRepository.findById(id);
+        if (!optionalPolicy.isPresent()) {
+            throw new DataCatalogPoliciesNotFoundException(String.format("Cannot find Policy with id: %s", id));
+        }
+        return optionalPolicy.get();
     }
 
     @ApiOperation(value = "Dekete Policy", tags = { "Policies" })
@@ -64,7 +80,7 @@ public class PolicyRestController {
             @ApiResponse(code = 500, message = "Internal server error")})
     @DeleteMapping("/policy/{id}")
     public void deletePolicy(@PathVariable Long id) {
-        service.deletePolicy(id);
+        policyRepository.deleteById(id);
     }
 
     @ApiOperation(value = "Update Policy", tags = { "Policies" })
@@ -74,7 +90,8 @@ public class PolicyRestController {
             @ApiResponse(code = 500, message = "Internal server error")})
     @PutMapping("/policy/{id}")
     public Policy updatePolicy(@PathVariable Long id, @Valid @RequestBody PolicyRequest policyRequest) {
-        return service.updatePolicy(id, policyRequest);
+        Policy policy = mapper.mapRequestToPolicy(policyRequest, id);
+        return policyRepository.save(policy);
     }
 
     @ApiOperation(value = "Get all Purposes", tags = { "Policies" })
@@ -84,7 +101,7 @@ public class PolicyRestController {
             @ApiResponse(code = 500, message = "Internal server error")})
     @GetMapping("/purpose")
     public List<Purpose> getPurposes() {
-        return service.getPurposes();
+        return purposeRepository.findAll();
     }
 
     @ApiOperation(value = "Get all Legal bases", tags = { "Policies" })
@@ -94,6 +111,6 @@ public class PolicyRestController {
             @ApiResponse(code = 500, message = "Internal server error")})
     @GetMapping("/legalbasis")
     public List<LegalBasis> getLegalBasis() {
-        return service.getLegalBases();
+        return legalBasisRepository.findAll();
     }
 }
--- data-catalog-policies-test/data-catalog-policies-test-component/src/test/java/no/nav/data/catalog/policies/test/component/mapper/PolicyMapperTest.java
+++ data-catalog-policies-test/data-catalog-policies-test-component/src/test/java/no/nav/data/catalog/policies/test/component/mapper/PolicyMapperTest.java
@@ -1,4 +1,4 @@
-package no.nav.data.catalog.policies.test.component.service.mapper;
+package no.nav.data.catalog.policies.test.component.mapper;
 
 import no.nav.data.catalog.policies.app.common.exceptions.DataCatalogPoliciesNotFoundException;
 import no.nav.data.catalog.policies.app.policy.PolicyRequest;
@@ -7,7 +7,7 @@ import no.nav.data.catalog.policies.app.policy.entities.Policy;
 import no.nav.data.catalog.policies.app.policy.entities.Purpose;
 import no.nav.data.catalog.policies.app.policy.repository.LegalBasisRepository;
 import no.nav.data.catalog.policies.app.policy.repository.PurposeRepository;
-import no.nav.data.catalog.policies.app.policy.service.mapper.PolicyMapper;
+import no.nav.data.catalog.policies.app.policy.mapper.PolicyMapper;
 import no.nav.data.catalog.policies.test.component.ComponentTestConfig;
 import org.junit.After;
 import org.junit.Before;
@@ -20,7 +20,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static no.nav.data.catalog.policies.test.component.service.PolicyServiceTest.*;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -34,6 +33,10 @@ public class PolicyMapperTest {
     private PurposeRepository purposeRepository;
     @Autowired
     private LegalBasisRepository legalBasisRepository;
+
+    public static final String LEGAL_BASIS_DESCRIPTION1 = "Legal basis 1";
+    public static final String PURPOSE_CODE1 = "PUR1";
+    public static final String PURPOSE_DESCRIPTION1 = "Purpose 1";
 
     @Rule
     public ExpectedException expectedException = ExpectedException.none();
