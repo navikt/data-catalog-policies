4f8bc676fef8b58530394f8d916a4d6698c42188
Ketill Fenne <ketill.fenne@gmail.com>
2019-05-08 10:47:49 +0200

    Added @Configuration (needed?)

--- data-catalog-policies-app/src/main/java/no/nav/data/catalog/policies/app/common/security/SecurityConfig.java
+++ data-catalog-policies-app/src/main/java/no/nav/data/catalog/policies/app/common/security/SecurityConfig.java
@@ -2,11 +2,13 @@ package no.nav.data.catalog.policies.app.common.security;
 
 import com.microsoft.azure.spring.autoconfigure.aad.AADAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@Configuration
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
