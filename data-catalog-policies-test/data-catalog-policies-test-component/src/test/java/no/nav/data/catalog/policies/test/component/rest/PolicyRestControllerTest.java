750a628a61a6e85c3e9ae128b4c293e11de8728a
Ketill Fenne <ketill.fenne@gmail.com>
2019-05-09 09:56:21 +0200

    unused import

--- data-catalog-policies-test/data-catalog-policies-test-component/src/test/java/no/nav/data/catalog/policies/test/component/rest/PolicyRestControllerTest.java
+++ data-catalog-policies-test/data-catalog-policies-test-component/src/test/java/no/nav/data/catalog/policies/test/component/rest/PolicyRestControllerTest.java
@@ -35,7 +35,6 @@ import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
