b998d48202f6fe2e19a43ec7026dd9d86b8311da
Ketill Fenne <ketill.fenne@gmail.com>
2019-06-06 11:24:18 +0200

    Fjernet ubrukte imports

--- data-catalog-policies-app/src/main/java/no/nav/data/catalog/policies/app/policy/rest/PolicyRestController.java
+++ data-catalog-policies-app/src/main/java/no/nav/data/catalog/policies/app/policy/rest/PolicyRestController.java
@@ -25,7 +25,6 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 import java.util.function.Function;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.toList;
@@ -77,7 +76,6 @@ public class PolicyRestController {
             return policyRepository.findByInformationTypeInformationTypeId(pageable, informationTypeId).map(policy -> mapper.mapPolicyToRequest(policy));
         }
     }
-
 
     @ApiOperation(value = "Create Policy", tags = { "Policies" })
     @ApiResponses(value = {
