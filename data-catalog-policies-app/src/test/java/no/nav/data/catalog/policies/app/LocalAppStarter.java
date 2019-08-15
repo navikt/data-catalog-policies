8777f5e6f4a499852e3fc65ebd84b545ef472e7c
Andreas Skomedal <andreas.skomedal@nav.no>
2019-08-15 13:45:57 +0200

    Revert "remove vault envclass config"

--- data-catalog-policies-app/src/test/java/no/nav/data/catalog/policies/app/LocalAppStarter.java
+++ data-catalog-policies-app/src/test/java/no/nav/data/catalog/policies/app/LocalAppStarter.java
@@ -4,6 +4,8 @@ public class LocalAppStarter {
 
     public static void main(String[] args) {
         System.setProperty("spring.profiles.active", "local");
+        System.setProperty("ENVIRONMENT_CLASS", "preprod");
+        System.setProperty("NAIS_NAMESPACE", "default");
         AppStarter.main(args);
     }
 }
