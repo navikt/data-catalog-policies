db40caced47c2e954081850555178b9e327d612f
Andreas Skomedal <andreas.skomedal@nav.no>
2019-08-15 13:24:23 +0200

    fix vaultconfig

--- data-catalog-policies-app/src/test/java/no/nav/data/catalog/policies/app/LocalAppStarter.java
+++ data-catalog-policies-app/src/test/java/no/nav/data/catalog/policies/app/LocalAppStarter.java
@@ -4,8 +4,6 @@ public class LocalAppStarter {
 
     public static void main(String[] args) {
         System.setProperty("spring.profiles.active", "local");
-        System.setProperty("ENVIRONMENT_CLASS", "preprod");
-        System.setProperty("NAIS_NAMESPACE", "default");
         AppStarter.main(args);
     }
 }
