d3c42fa930f609130e8ca8bc578378d3cc8df72e
Andreas Skomedal <andreas.skomedal@nav.no>
2019-09-10 14:06:07 +0200

    manual db credential vault rotation

--- data-catalog-policies-app/src/main/java/no/nav/data/catalog/policies/app/common/jpa/FlywayConfig.java
+++ data-catalog-policies-app/src/main/java/no/nav/data/catalog/policies/app/common/jpa/FlywayConfig.java
@@ -2,13 +2,16 @@ package no.nav.data.catalog.policies.app.common.jpa;
 
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.flyway.FlywayConfigurationCustomizer;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.cloud.vault.config.databases.VaultDatabaseProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.vault.core.VaultOperations;
-import org.springframework.vault.core.lease.SecretLeaseContainer;
+import org.springframework.vault.support.VaultResponse;
+
+import java.util.Map;
 
 import static java.lang.String.format;
 
@@ -19,24 +22,25 @@ public class FlywayConfig {
 
     @Bean
     public FlywayConfigurationCustomizer flywayConfigurationCustomizer(
-            SecretLeaseContainer secretLeaseContainer,
             VaultOperations vaultOperations,
-            @Value("${spring.datasource.url}") String url,
-            @Value("${spring.cloud.vault.database.role}") String userRole,
-            @Value("${spring.cloud.vault.database.backend}") String backend
+            VaultDatabaseProperties vaultDatabaseProperties,
+            DataSourceProperties dataSourceProperties
     ) {
         return configuration -> {
+            String userRole = vaultDatabaseProperties.getRole();
+            String backend = vaultDatabaseProperties.getBackend();
             String adminRole = userRole.replace("-user", "-admin");
-            val secretPath = format("%s/creds/%s", backend, adminRole);
+            String secretPath = format("%s/creds/%s", backend, adminRole);
 
-            val vaultResponse = vaultOperations.read(secretPath);
-            val username = vaultResponse.getData().get("username").toString();
-            val password = vaultResponse.getData().get("password").toString();
+            VaultResponse vaultResponse = vaultOperations.read(secretPath);
+            Map<String, Object> data = vaultResponse.getData();
+            val username = data.get("username").toString();
+            val password = data.get("password").toString();
 
             log.info("Vault: Flyway configured with credentials from Vault. Credential path: {}", secretPath);
 
             configuration
-                    .dataSource(url, username, password)
+                    .dataSource(dataSourceProperties.getUrl(), username, password)
                     .initSql(String.format("SET ROLE \"%s\"", adminRole));
         };
     }
