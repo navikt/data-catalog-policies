2dd4180a1ddbdd9d4c94dac5a7adfa5cf8464212
Andreas Skomedal <andreas.skomedal@nav.no>
2019-09-17 16:08:36 +0200

    Dont fail return on not found dataset

--- data-catalog-policies-app/src/main/java/no/nav/data/catalog/policies/app/policy/mapper/PolicyMapper.java
+++ data-catalog-policies-app/src/main/java/no/nav/data/catalog/policies/app/policy/mapper/PolicyMapper.java
@@ -1,6 +1,7 @@
 package no.nav.data.catalog.policies.app.policy.mapper;
 
 import lombok.extern.slf4j.Slf4j;
+import no.nav.data.catalog.policies.app.common.exceptions.DataCatalogPoliciesNotFoundException;
 import no.nav.data.catalog.policies.app.consumer.CodelistConsumer;
 import no.nav.data.catalog.policies.app.consumer.DatasetConsumer;
 import no.nav.data.catalog.policies.app.policy.domain.CodeResponse;
@@ -40,7 +41,7 @@ public class PolicyMapper {
         try {
             Dataset dataset = datasetConsumer.getDatasetById(policy.getDatasetId());
             response.setDataset(dataset);
-        } catch (Exception e) {
+        } catch (DataCatalogPoliciesNotFoundException e) {
             log.trace(String.format("Cannot find Dataset with id: %s", policy.getDatasetId()));
         }
         response.setPurpose(new CodeResponse(policy.getPurposeCode(), codelistConsumer.getCodelistDescription(ListName.PURPOSE, policy.getPurposeCode())));
