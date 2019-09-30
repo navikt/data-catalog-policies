14ae16d316545711c70953ecb33554c4f588bdd7
Andreas Skomedal <andreas.skomedal@nav.no>
2019-09-30 16:31:46 +0200

    swagger auth config

--- data-catalog-policies-app/src/main/java/no/nav/data/catalog/policies/app/common/swagger/SwaggerConfig.java
+++ data-catalog-policies-app/src/main/java/no/nav/data/catalog/policies/app/common/swagger/SwaggerConfig.java
@@ -1,20 +1,29 @@
 package no.nav.data.catalog.policies.app.common.swagger;
 
+import io.swagger.models.auth.In;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import springfox.documentation.builders.AuthorizationScopeBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Collections;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
@@ -22,7 +31,17 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.basePackage("no.nav.data.catalog.policies.app.policy.rest"))
                 .paths(PathSelectors.any())
                 .build()
+                .securitySchemes(List.of(new ApiKey("Token Access", HttpHeaders.AUTHORIZATION, In.HEADER.name())))
+                .securityContexts(List.of(SecurityContext.builder().securityReferences(tokenAccess()).forPaths(PathSelectors.any()).build()))
                 .apiInfo(apiInfo());
+    }
+
+    private List<SecurityReference> tokenAccess() {
+        return List.of(SecurityReference.builder()
+                .reference("Token Access")
+                .scopes(new AuthorizationScope[]{
+                        new AuthorizationScopeBuilder().scope("global").build()
+                }).build());
     }
 
     private ApiInfo apiInfo() {
