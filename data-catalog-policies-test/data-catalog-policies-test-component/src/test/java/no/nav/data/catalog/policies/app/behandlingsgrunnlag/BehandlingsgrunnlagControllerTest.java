8014290a69b1ea6cc3f85f0e57c2d6a54dc8ae17
Andreas Skomedal <andreas.skomedal@nav.no>
2019-10-01 14:35:22 +0200

    add behandlingsgrunnlag controller test

---
+++ data-catalog-policies-test/data-catalog-policies-test-component/src/test/java/no/nav/data/catalog/policies/app/behandlingsgrunnlag/BehandlingsgrunnlagControllerTest.java
@@ -0,0 +1,45 @@
+package no.nav.data.catalog.policies.app.behandlingsgrunnlag;
+
+import no.nav.data.catalog.policies.app.AppStarter;
+import no.nav.data.catalog.policies.app.policy.domain.Dataset;
+import no.nav.data.catalog.policies.app.policy.repository.PolicyRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(BehandlingsgrunnlagController.class)
+@ContextConfiguration(classes = AppStarter.class)
+@ActiveProfiles("test")
+class BehandlingsgrunnlagControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private PolicyRepository policyRepository;
+
+    @Test
+    void hentBehandlingsgrunnlag() throws Exception {
+        given(policyRepository.findDatasetsByPurposeCode("the-purpose")).willReturn(List.of(new Dataset("id", "title")));
+
+        mvc.perform(get("/behandlingsgrunnlag/purpose/{purpose}", "the-purpose"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.purpose", is("the-purpose")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.datasets[0].id", is("id")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.datasets[0].title", is("title")));
+    }
+}
